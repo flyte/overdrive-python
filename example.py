c32652f66ae009adc235fc10f1f0476503922390
Ellis Percival <flyte@failcode.co.uk>
2019-01-10 19:58:01 +0000

    Update example

--- example.py
+++ example.py
@@ -1,12 +1,11 @@
 from overdrive import Overdrive
 
-def locationChangeCallback(addr, location, piece, speed, clockwise):
+def locationChangeCallback(addr, params):
     # Print out addr, piece ID, location ID of the vehicle, this print everytime when location changed
-    print("Location from " + addr + " : " + "Piece=" + str(piece) + " Location=" + str(location) + " Clockwise=" + str(clockwise))
-
+    print("Location from {addr} : Piece={piece} Location={location} Clockwise={clockwise}".format(addr=addr, **params))
 
 car = Overdrive("xx:xx:xx:xx:xx:xx")
 car.setLocationChangeCallback(locationChangeCallback) # Set location change callback to function above
-car.changeSpeed(500, 1000) # Set car speed with speed = 500, acceleration = 1000
-car.changeLaneRight(1000, 1000) # Switch to next right lane with speed = 1000, acceleration = 1000
+car.changeSpeed(500, 500) # Set car speed with speed = 500, acceleration = 500
+car.changeLaneRight(500, 500) # Switch to next right lane with speed = 500, acceleration = 500
 input() # Hold the program so it won't end abruptly
